49a70cb675a0c543fb9aabb41ad78eab36ff3b70
Olivier Castro-Perrier <olivier.cperrier@hotmail.com>
2022-01-07 16:53:43 -0500

    SJIP-17: wip

---
+++ migrations/1641590666695_add-more-user-data-column.sql
@@ -0,0 +1,17 @@
+-- Up Migration
+ALTER TABLE users 
+    ADD COLUMN era_commons_id VARCHAR(255),
+    ADD COLUMN nih_ned_id VARCHAR(255),
+    ADD COLUMN occupation TEXT[],
+    ADD COLUMN affiliation TEXT,
+    ADD COLUMN research_area TEXT,
+    ADD COLUMN portal_usages TEXT[]
+
+-- Down Migration
+ALTER TABLE users 
+    DROP COLUMN era_commons_id,
+    DROP COLUMN nih_ned_id,
+    DROP COLUMN occupation,
+    DROP COLUMN affiliation,
+    DROP COLUMN research_area,
+    DROP COLUMN portal_usages
--- src/data/userType.ts
+++ src/data/userType.ts
@@ -1,6 +1,14 @@
 export type TUser = {
     id: number;
     keycloak_id: string;
+    first_name?: string;
+    last_name?: string;
+    era_commons_id?: string;
+    nih_ned_id?: string;
+    occupation?: string[];
+    affiliation?: string;
+    research_area?: string;
+    portal_usages?: string[];
     creation_date: Date;
     updated_date: Date;
     consent_date?: Date;

70dbcae8699c0d9e7f188ad3a4934c2299361e88
Olivier Castro-Perrier <olivier.cperrier@hotmail.com>
2022-01-27 14:28:11 -0500

    Set uuid

--- migrations/1643307922961_add-saved-filters.sql
+++ migrations/1643307922961_add-saved-filters.sql
@@ -1,6 +1,6 @@
 -- Up Migration
 CREATE TABLE saved_filters (
-    id SERIAL NOT NULL,
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
     keycloak_id VARCHAR(255) NOT NULL,
     title TEXT,
     tag TEXT,
@@ -8,6 +8,7 @@ CREATE TABLE saved_filters (
     creation_date TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     updated_date TIMESTAMP WITHOUT TIME ZONE NOT NULL
 );
+ALTER TABLE saved_filters ADD PRIMARY KEY (id);
 
 -- Down Migration
 DROP TABLE saved_filters;

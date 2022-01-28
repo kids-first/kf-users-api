c6512722cb9898276e92b6018b23907965fb81da
Olivier Castro-Perrier <olivier.cperrier@hotmail.com>
2022-01-28 10:26:38 -0500

    Fix migration

--- migrations/1643307922961_add-saved-filters.sql
+++ migrations/1643307922961_add-saved-filters.sql
@@ -1,4 +1,5 @@
 -- Up Migration
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE saved_filters (
     id uuid DEFAULT gen_random_uuid() NOT NULL,
     keycloak_id VARCHAR(255) NOT NULL,
@@ -12,3 +13,4 @@ ALTER TABLE saved_filters ADD PRIMARY KEY (id);
 
 -- Down Migration
 DROP TABLE saved_filters;
+DROP EXTENSION "uuid-ossp";
--- migrations/1643381363522_add-uuid-extension.sql
+++
@@ -1,9 +0,0 @@
--- Up Migration
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-ALTER TABLE ONLY saved_filters ALTER COLUMN id SET DEFAULT uuid_generate_v4();
-
--- Down Migration
-DROP EXTENSION "uuid-ossp";
-
-ALTER TABLE ONLY saved_filters ALTER COLUMN id SET DEFAULT gen_random_uuid();

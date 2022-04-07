10af01f9f846b1f076e14770144994ee228a7f91
adrian <adipaul1981@yahoo.ca>
2022-04-07 13:48:59 -0400

    delete last up (failed)

--- migrations/1649352058166_rename-usersets-date-columns.sql
+++
@@ -1,9 +0,0 @@
--- Up Migration
-ALTER TABLE user_sets
-    RENAME COLUMN creation_date TO creationDate;
-    RENAME COLUMN updated_date TO updatedDate;
-
--- Down Migration
-ALTER TABLE user_sets
-    RENAME COLUMN creationDate TO creation_date;
-    RENAME COLUMN updatedDate TO updated_date;

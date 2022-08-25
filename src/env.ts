fd6b8df5a77a53126e1f2ec9d4226797cf3db074
Evans Girard <54366437+evans-g-crsj@users.noreply.github.com>
2022-08-24 21:38:05 -0400

    :wrench: Remove some env vars (#5)

--- src/env.ts
+++ src/env.ts
@@ -8,8 +8,8 @@ export const keycloakURL = process.env.KEYCLOAK_URL;
 export const keycloakRealm = process.env.KEYCLOAK_REALM;
 export const keycloakClient = process.env.KEYCLOAK_CLIENT;
 
-export const dbHost = process.env.DATABASE_HOST || process.env.PGHOST;
-export const dbPort: number = Number.parseInt(process.env.DATABASE_PORT || process.env.PGPORT);
-export const dbName = process.env.DATABASE_NAME || process.env.PGDATABASE;
-export const dbUser = process.env.DATABASE_USER || process.env.PGUSER;
-export const dbPassword = process.env.DATABASE_PASSWORD || process.env.PGPASSWORD;
+export const dbHost = process.env.PGHOST;
+export const dbPort: number = Number.parseInt(process.env.PGPORT);
+export const dbName = process.env.PGDATABASE;
+export const dbUser = process.env.PGUSER;
+export const dbPassword = process.env.PGPASSWORD;
